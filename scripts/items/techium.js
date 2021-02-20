0eeeea195cea129b0d9da7ca639aae592cbb7f75
THEGAMER-10n <jlupinman@gmail.com>
2021-02-20 17:39:15 +0800

    h

--- scripts/items/techium.js
+++ scripts/items/techium.js
@@ -1,7 +1,11 @@
+
 const techium = Item("techium");
 
 techium.localizedName = "Techium"
 techium.description = "The raw form of all techium technologies. Highly explosive.";
+
 techium.radioactivity = 0.65;
 techium.explosiveness = 1;
 techium.charge = 0.75;
+
+TechTree.TechNode(Item.titanium,techium,ItemStack.with(Item.copper,1))
