ee301d0bc3e2168e6b16c768567e7ad8f742ba03
THEGAMER-10n <jlupinman@gmail.com>
2021-02-22 16:32:10 +0800

    h

--- scripts/items.js
+++ scripts/items.js
@@ -2,16 +2,9 @@
 let f = require("scripts/functions")
 //---------------------------------------------------------------------------
 const techium = Item("techium");
-
 techium.localizedName = "Techium"
 techium.description = "The raw form of all techium technologies. Highly explosive.";
-
 techium.radioactivity = 0.65;
 techium.explosiveness = 1;
 techium.charge = 0.75;
-
 techium.color = Color.valueOf("#88A7B8")
-
-f.techNode(Blocks.coreShard,techium,ItemStack.with(
-    Items.copper,1
-));
