23321947e18e1bf6ec5d6a23865f253cb812e46f
THEGAMER-10n <jlupinman@gmail.com>
2021-02-22 17:11:03 +0800

    h

--- scripts/items.js
+++ scripts/items.js
@@ -1,10 +1,19 @@
 //imports
-let f = require("scripts/functions")
-//---------------------------------------------------------------------------
-const techium = Item("techium");
-techium.localizedName = "Techium"
-techium.description = "The raw form of all techium technologies. Highly explosive.";
-techium.radioactivity = 0.65;
-techium.explosiveness = 1;
-techium.charge = 0.75;
-techium.color = Color.valueOf("#88a7b8")
+const res = require("functions");
+//functions
+function newItem(name,color,cost,flamability,radioactivity,explosiveness,charge){
+    const item = extendContent(Item, name, {});
+    item.color = Color.valueOf(color);
+    item.cost = cost
+    item.flamability = flamability
+    item.radioactivity = radioactivity
+    item.explosiveness = explosiveness
+    item.charge = charge
+}
+//techium
+const techium = newItem("techium","#8dabc9",0.0,0.0,0.75,1,0.5);
+const pTechium = newItem("pTechium","#4e8cca",0.0,0.0,0.60,0.45,0.6);
+const fTechium = newItem("fTechium","#27598b",0.0,0.0,0.25,0.0,0.75);
+const sTecium = newItem("sTechium","#0057ad",0.0,0.0,0.0,0.0,1);
+//research
+res.techNode(Block.coreShard,techium,ItemStack.with(Items.copper,1));
