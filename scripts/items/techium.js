87c40ff39b47c2f1242c473e3b570c7bad7830f7
THEGAMER-10n <jlupinman@gmail.com>
2021-02-22 09:30:20 +0800

    h

--- scripts/items/techium.js
+++ scripts/items/techium.js
@@ -1,5 +1,5 @@
 //imports
-let research = require("scripts/research")
+let r = require("scripts/research")
 //------------
 const techium = Item("techium");
 
@@ -10,6 +10,6 @@ techium.radioactivity = 0.65;
 techium.explosiveness = 1;
 techium.charge = 0.75;
 
-techium.color = Color.valueOf(" #88A7B8")
+techium.color = Color.valueOf("#88A7B8")
 
-research.research1(Blocks.coreShard,techium,ItemStack.with(Items.copper,1));
+r.research1(Blocks.coreShard,techium,ItemStack.with(Items.copper,1));
--- scripts/research.js
+++ scripts/research.js
@@ -1,4 +1,4 @@
-research.exports = {
+main.exports = {
     research1(parent2, block, requirements){
         var parent = TechTree.all.find(node => node.content == parent2);
         var node = new TechTree.TechNode(parent, block, requirements);
