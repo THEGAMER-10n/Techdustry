e13a4841b96b44f6e597339935cf49954386dda9
THEGAMER-10n <jlupinman@gmail.com>
2021-02-25 09:44:28 +0800

    kronos test

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -3,26 +3,5 @@ let item = require("items");
 let a = require("ammo");
 
 //the actual turret
-const kronos = extend(ItemTurret,"kronos",{
-    health: 3400,
-    size: 4,
-    range: 60*8,
-    reloadSpeed: 5*60,
-    rotateSpeed: 1.5,
-    targetAir: true,
-    targetGround: true,
-    recoil: 5,
-    restitution: 1,
-    maxAmmo: 10,
-    ammoPerShot: 5,
-    requirements: ItemStack.with(
-        item.techium,560,
-        item.pTechium,890,
-        item.sTechium,120,
-        Items.lead,980,
-        Items.silicon,765,
-        Items.titanium,450,
-        Items.thorium,685
-    ),
-    category: Category.turret
-});
+const kronos = extend(ItemTurret,"kronos",{});
+kronos.health = 50;
