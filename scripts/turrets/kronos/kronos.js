e262fe2c82e1ea5343b7d55b1732e565f432482d
THEGAMER-10n <jlupinman@gmail.com>
2021-03-02 12:22:21 +0800

    KRONOS

--- scripts/turrets/kronos/kronos.js
+++ scripts/turrets/kronos/kronos.js
@@ -13,9 +13,12 @@ const kronos = extend(ItemTurret,"kronos",{
     inaccuracy: 0.05,
     recoilAmount: 8,
     restitution: 0.002,
-    buildVisibility: buildVisibility.shown,
+    buildVisibility: BuildVisibility.shown,
     requirements: ItemStack.with(Items.copper,999999999),
     category: Category.turret,
     shots: 1,
+    shootEffect: Fx.none,
+    ammoUseEffect: Fx.none,
+    smokeEffect: Fx.none
 })
 //I'm still gonna implement ammos only l8er
