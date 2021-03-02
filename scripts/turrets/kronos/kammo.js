e35050ec6f63b81c41b9bf9df15df9a18d30fcc7
THEGAMER-10n <jlupinman@gmail.com>
2021-03-02 16:34:55 +0800

    ,-,

--- scripts/turrets/kronos/kammo.js
+++ scripts/turrets/kronos/kammo.js
@@ -1,12 +1,13 @@
 //Ammo
 var calc = require("calculations");
 var stats = require("status");
+
 const techium = extend(BasicBulletType,"techium",{
     damage: 900,
     speed: 5,
     lifetime: calc.life(60,5),
-    status: stats.shortCircuit,
-    statusDuration: 60,
     pierce: true,
     pierceCap: 3,
 });
+techium.status = stats.shortCircuit;
+techium.statusDuration = 60;
