d0ebba6f0cedc9cbe97a7e56763d6ecc6e797fb5
THEGAMER-10n <jlupinman@gmail.com>
2021-03-02 11:50:17 +0800

    trying to fix kronos

--- scripts/turrets/kronos/kronos.js
+++ scripts/turrets/kronos/kronos.js
@@ -7,11 +7,14 @@ const kronos = extend(ItemTurret,"kronos",{
     description: "TO-DO",
     health: 2045,
     range: 400,
+    size: 4,
     reloadSpeed: 120,
     rotatespeed: 2,
     inaccuracy: 0.05,
     recoil: 8,
     restitution: 0.002,
-    buildVisibility: buildVisibility.shown
+    buildVisibility: buildVisibility.shown,
+    requirements: ItemStack.with(Items.copper,999999999),
+    category: Category.turret
 })
 //I'm still gonna implement ammos only l8er
