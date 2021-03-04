02b083131ed8559e97bdb4be1b1f35e9ef9c0864
THEGAMER-10n <jlupinman@gmail.com>
2021-03-04 17:58:57 +0800

    kronos fix again..

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,4 +1,4 @@
-const shortCircuit = extend(StatusEffect,"sC",{
+const shortCircuit = extend(StatusEffect,{
     damage: Math.round(Math.random()*10),
     armorMultiplier: Math.round(Math.random()*2)
 })
